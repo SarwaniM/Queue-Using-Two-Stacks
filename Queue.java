e0a5909e5d6d666645fdf93457c0471887b6754a
SarwaniM <sarwanim@msitprogram.net>
2022-02-17 14:00:58 +0530

    empty

--- Queue.java
+++ Queue.java
@@ -37,7 +37,7 @@ class Queue {
     
     // Return whether the queue is empty.
     public boolean empty() {
-
+        return stack1.isEmpty();
     }
 
     // Return the number of elements in queue.
