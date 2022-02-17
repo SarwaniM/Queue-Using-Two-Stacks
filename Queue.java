d74f1211e7635542c24cee81fd60b897be842d9a
SarwaniM <sarwanim@msitprogram.net>
2022-02-17 14:04:23 +0530

    changes made in main method to scan input files

--- Queue.java
+++ Queue.java
@@ -45,8 +45,9 @@ class Queue {
         return stack1.size();
     }
     
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader f= new FileReader(args[0]);
+        Scanner scan = new Scanner(f);
         Queue queue = new Queue();
         int queries = Integer.parseInt(scan.nextLine());
         for(int i = 0; i < queries; i++) {
